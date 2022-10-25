69b31cdd3ea76ad4ff1d97a56aada8bde3fac6a5
Erick140297 <erickmonterrubio67@outlook.com>
2022-10-25 06:31:42 -0500

    falta paginado y filtros

--- client/src/components/Exito/Exito.jsx
+++ client/src/components/Exito/Exito.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import s from "./Exito.module.css";
+
 
 export default function Exito() {
   return (
-    <div>
-      <h2>¡Felicidades tu pokemon ha sido creado con Exito!</h2>
-      <Link to={"/home/pokemons"}>
-        <button>Home</button>
+    <div className={s.container}>
+      <h2 className={s.h2}>¡Felicidades tu pokemon ha sido creado con Exito!</h2>
+      <Link to={"/home/pokemons"} style={{textDecoration: 'none'}}>
+        <button className={s.boton} >Home</button>
       </Link>
     </div>
   );
